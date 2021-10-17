e20e7739beeb3737225aaebf627beca38acbdbea
Chris Pesar <pesarcomputer@Chriss-MacBook-Air.local>
2021-10-17 16:12:30 -0600

    validated user inputs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function validate(validatableInput) {
+    var isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value > validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value < validatableInput.max;
+    }
+    return isValid;
+}
 // Autobind Decorator
 // function autobind(
 //     _: any, 
@@ -32,9 +55,28 @@ var ProjectInput = /** @class */ (function () {
         var enteredTitle = this.titleInputElement.value;
         var enteredDescription = this.descriptionInputElement.value;
         var enteredPeople = this.peopleInputElement.value;
-        if (enteredTitle.trim().length === 0 ||
-            enteredDescription.trim().length === 0 ||
-            enteredPeople.trim().length === 0) {
+        var titleValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        var descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        var peopleValidatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1
+        };
+        // Validation
+        if (
+        // enteredTitle.trim().length === 0 || 
+        // enteredDescription.trim().length === 0 || 
+        // enteredPeople.trim().length === 0
+        !validate(titleValidatable) &&
+            !validate(descriptionValidatable) &&
+            !validate(peopleValidatable)) {
             alert('Invalid input, please try again!');
             return;
         }
