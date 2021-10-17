ad7fd90ec80eb0782d6408c0106f4b727ea65bc4
Chris Pesar <pesarcomputer@Chriss-MacBook-Air.local>
2021-10-17 16:29:40 -0600

    created project list sections

--- src/app.js
+++ src/app.js
@@ -37,6 +37,29 @@ function validate(validatableInput) {
 //     };
 //     return adjDescriptor;
 // }
+// ProjectList Class 
+var ProjectList = /** @class */ (function () {
+    function ProjectList(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = this.type + "-projects";
+        this.attach();
+        this.renderContent();
+    }
+    ProjectList.prototype.renderContent = function () {
+        var listId = this.type + "-project-list";
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' PROJECTS';
+    };
+    ProjectList.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    };
+    return ProjectList;
+}());
 // Project Input Class- Object oreinted approach
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
@@ -110,3 +133,5 @@ var ProjectInput = /** @class */ (function () {
     return ProjectInput;
 }());
 var prjInput = new ProjectInput();
+var activePrjList = new ProjectList('active');
+var finishedPrjList = new ProjectList('finished');
