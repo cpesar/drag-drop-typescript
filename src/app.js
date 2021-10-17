940ee69133b40379dbb0c638869eebf21f66b0f1
Chris Pesar <pesarcomputer@Chriss-MacBook-Air.local>
2021-10-17 15:52:31 -0600

    user input printing to console

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 // Autobind Decorator
-function autobind(_, _2, descriptor) {
-    var originalMethod = descriptor.value;
-    var adjDescriptor = {
-        configurable: true,
-        get: function () {
-            var boundFn = originalMethod.bind(this);
-            return boundFn;
-        }
-    };
-    return adjDescriptor;
-}
+// function autobind(
+//     _: any, 
+//     _2: string, 
+//     descriptor: TypedPropertyDescriptor<any>
+//     ) { 
+//     const originalMethod = descriptor.value;
+//     const adjDescriptor: PropertyDescriptor = {
+//         configurable: true,
+//         get(){
+//             const boundFn = originalMethod.bind(this);
+//             return boundFn;
+//         }
+//     };
+//     return adjDescriptor;
+// }
 // Project Input Class- Object oreinted approach
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
@@ -30,9 +28,36 @@ var ProjectInput = /** @class */ (function () {
         this.configure();
         this.attach();
     }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var enteredDescription = this.descriptionInputElement.value;
+        var enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again!');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    };
+    ProjectInput.prototype.clearInputs = function () {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    };
+    // Error here for experimental decorators
+    // @autobind
     ProjectInput.prototype.submitHandler = function (event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        // console.log(this.titleInputElement.value);
+        var userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            var title = userInput[0], desc = userInput[1], people = userInput[2];
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     };
     ProjectInput.prototype.configure = function () {
         this.element.addEventListener('submit', this.submitHandler.bind(this));
@@ -40,9 +65,6 @@ var ProjectInput = /** @class */ (function () {
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
-    __decorate([
-        autobind
-    ], ProjectInput.prototype, "submitHandler");
     return ProjectInput;
 }());
 var prjInput = new ProjectInput();
