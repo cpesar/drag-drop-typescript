cac398f5786b4f43999dfb9f0aa09c623dc9d855
Chris Pesar <pesarcomputer@Chriss-MacBook-Air.local>
2021-10-17 15:25:26 -0600

    decorator error

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-// Object oreinted approach
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// Autobind Decorator
+function autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+// Project Input Class- Object oreinted approach
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = document.getElementById('project-input');
@@ -22,6 +40,9 @@ var ProjectInput = /** @class */ (function () {
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
+    __decorate([
+        autobind
+    ], ProjectInput.prototype, "submitHandler");
     return ProjectInput;
 }());
 var prjInput = new ProjectInput();
